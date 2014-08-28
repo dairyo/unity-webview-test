57de3e3b90a619ac6fdfe8e7e5673ac414c9a0b8
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2014-08-28 16:10:21 +0900

    Menu scene was added.

---
+++ webview-test/Assets/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    void OnGUI() {
+
+        //画面の左上にメニューを作る
+        GUI.Box (new Rect(10,10,100,90),"MENU");
+
+        if (GUI.Button(new Rect(30, 35, 60, 30), "Button"))
+        {
+            Debug.Log("Button pressed");
+        }
+    }
+
+}
