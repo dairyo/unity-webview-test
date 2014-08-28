ae057a2813b95cbdb8ceef17b08d43e48975d600
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2014-08-28 17:07:59 +0900

    Web view scene was added.
    Menu scene was removed.

---
+++ webview-test/Assets/WebViewScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebViewScript : MonoBehaviour {
+
+    public string Url;
+    WebViewObject webViewObject;
+    void Start() {
+        webViewObject =
+            (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+        webViewObject.Init ();
+        webViewObject.LoadURL(Url);
+        webViewObject.SetVisibility(true);
+    }
+}
