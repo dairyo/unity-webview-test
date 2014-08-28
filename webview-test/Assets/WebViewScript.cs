d1e9c808288c548fe74f42cdce7facef5d0e203e
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2014-08-28 19:25:59 +0900

    Fix.

--- webview-test/Assets/WebViewScript.cs
+++ webview-test/Assets/WebViewScript.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class WebViewScript : MonoBehaviour {
 
-    public string Url;
+    private string url = "http://www.google.com/";
     WebViewObject webViewObject;
+
     void Start() {
         webViewObject =
             (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
-        webViewObject.Init ();
-        webViewObject.LoadURL(Url);
+        webViewObject.Init((msg)=>{
+            if (msg == "clicked") {
+                Debug.Log("hogehogehogehoge");
+            }
+        });
+        webViewObject.LoadURL(url);
         webViewObject.SetVisibility(true);
+        webViewObject.EvaluateJS(
+            "window.addEventListener('load', function() {" +
+            "   window.addEventListener('click', function() {" +
+            "       Unity.call('clicked');" +
+            "   }, false);" +
+            "}, false);");
+        webViewObject.SetMargins(0, 0, 0, 100); //下に100pxマージンを取る
     }
 }
